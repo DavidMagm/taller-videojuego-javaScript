ee3de15b5c98a2512657ee375f57dc541acd5dc0
david torres <david.martinezezguerra@gmail.com>
2023-09-11 18:46:05 -0500

    organizando la logica de la extraccion y ubicacion de los emojis en el canvas

--- game.js
+++ game.js
@@ -8,11 +8,9 @@ let canvaSize;
 let elementSize;
 
 function setGameSize() {
-    if(window.innerHeight > window.innerWidth) {
-        canvaSize = window.innerWidth * 0.8;
-    } else {
-        canvaSize = window.innerHeight * 0.8;
-    }
+    window.innerHeight > window.innerWidth 
+    ? canvaSize = window.innerWidth * 0.8 
+    : canvaSize = window.innerHeight * 0.8;
     canvas.setAttribute('width',canvaSize);
     canvas.setAttribute('height',canvaSize);
     elementSize = (canvaSize / 10) -1;
@@ -22,7 +20,23 @@ function setGameSize() {
 function startGame() {
     game.font = `${elementSize}px Arial`;
     game.textAlign = "end";
-    for(let i = 1; i <= 10; i++) {
-        game.fillText(emojis['X'], elementSize * i, elementSize)
-    }
+
+    const map = maps[0];
+    const mapRow = map.trim().split('\n');
+    const mapRowCol = mapRow.map(row => row.trim().split(''));
+
+    mapRowCol.forEach((row, rowIndex) => {
+        row.forEach((col,colIndex) => {
+            const emoji = emojis[col];
+            const posX = elementSize * (colIndex + 1);
+            const posY = elementSize * (rowIndex + 1);
+            game.fillText(emoji, posX, posY);
+        })
+    });
+
+    // for(let row = 1; row < 10; row++) {
+    //     for(let col = 1; col <= 10; col++) {
+    //         game.fillText(emojis[mapRowCol[row - 1][col - 1]], elementSize * col, elementSize * row)
+    //     }
+    // }
 }
