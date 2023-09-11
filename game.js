a24f3a390c801459bc941b1175a01e7b3ca94dfa
david torres <david.martinezezguerra@gmail.com>
2023-09-11 15:35:58 -0500

    estilos y logica para insertar emojis de bombas

--- game.js
+++ game.js
@@ -1,1 +1,28 @@
-console.log(maps);
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2D');
+
+window.addEventListener('load', setGameSize);
+window.addEventListener('resize', setGameSize);
+
+let canvaSize;
+let elementSize;
+
+function setGameSize() {
+    if(window.innerHeight > window.innerWidth) {
+        canvaSize = window.innerWidth * 0.8;
+    } else {
+        canvaSize = window.innerHeight * 0.8;
+    }
+    canvas.setAttribute('width',canvaSize);
+    canvas.setAttribute('height',canvaSize);
+    elementSize = (canvaSize / 10) -1;
+    startGame()
+}
+
+function startGame() {
+    game.font = `${elementSize}px Arial`;
+    game.textAlign = "end";
+    for(let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'], elementSize * i, elementSize)
+    }
+}
